8d025bd22d9de710fa92edb4c40dc86f4cc77857
Christopher Posada <chris@cpsharp.net>
2016-09-30 23:48:11 -0400

    film reel

--- fs.php
+++ fs.php
@@ -1,100 +1,107 @@
 <?php
 require_once __DIR__ . '/config.php';
 
-function create_meta() {
-	$vid_list = new VideoCollection();
+function create_meta()
+{
+    $vid_list = new VideoCollection();
 
-	foreach ( VID_PATHS as $path ) {
-		$files     = scandir( $path );
-		$path_list = new VideoList( $path );
-		foreach ( $files as $file ) {
-			if ( strpos( $file, 'mkv' ) !== false || strpos( $file, 'mp4' ) !== false ) {
-				$path_list->add( $file );
-			}
-		}
+    foreach (VID_PATHS as $path) {
+        $files = scandir($path);
+        $path_list = new VideoList($path);
+        foreach ($files as $file) {
+            if (strpos($file, 'mkv') !== false || strpos($file, 'mp4') !== false) {
+                $path_list->add($file);
+            }
+        }
 
-		$vid_list->add( $path_list );
-	}
+        $vid_list->add($path_list);
+    }
 
-	return $vid_list;
+    return $vid_list;
 }
 
-function force_download( $filename ) {
-	header( 'Content-Type: application/octet-stream' );
-	header( 'Content-Transfer-Encoding: Binary' );
-	header( 'Content-disposition: attachment; filename="' . basename( $filename ) . '"' );
-	readfile( $filename );
+class VideoList
+{
+    private $root_path;
+    private $file_list;
+    private $imagery = [];
+
+    public function __construct($root_path)
+    {
+        $this->root_path = $root_path;
+        $this->file_list = [];
+    }
+
+    public function add($file)
+    {
+        $this->file_list[] = $file;
+    }
+
+    public function draw()
+    {
+        $list = '<ul class="rolldown-list" id="myList">';
+
+        foreach ($this->file_list as $f_name) {
+            $list .= '<li>';
+            $file_path = $this->root_path . '/' . $f_name;
+            $friendly_name = $this->get_friendly_name($f_name);
+            $this->imagery[] = str_replace('.', ' ', $friendly_name . '.jpg');
+            $list .= '<a href="' . $file_path . '" download><button class="download">' . $friendly_name . '</button></a>';
+            $list .= '</li >';
+        }
+        $list .= '</ul >';
+
+        return $list;
+    }
+
+    public function draw_old()
+    {
+        $list = '<ul class="rolldown-list" id="myList">';
+
+        foreach ($this->file_list as $file_name) {
+            $list .= '<li>';
+            $file_path = $this->root_path . '/' . $file_name;
+            $friendly_name = $this->get_friendly_name($file_name);
+            $this->imagery[] = str_replace('.', ' ', $friendly_name . '.jpg');
+            $list .= $friendly_name;
+            $list .= '<a href="' . $file_path . '" download>' . '<button class="download">Save</button></a>';
+            $list .= '</li >';
+        }
+        $list .= '</ul >';
+
+        return $list;
+    }
+
+    private function get_friendly_name($name)
+    {
+
+        $name = str_replace('mkv', '', $name);
+        $name = str_replace('.', ' ', $name);
+        $name = trim(str_replace('mp4', ' ', $name));
+        $names = explode(' ', $name);
+        $last_word = $names[count($names) - 1];
+        $top = str_replace($last_word, '', $name);
+
+        return $top . '<br />(' . $last_word.')';
+    }
+
+    public function getImagery()
+    {
+        return $this->imagery;
+    }
 }
 
-class VideoList {
-	private $root_path;
-	private $file_list;
-	private $imagery = [];
+class VideoCollection
+{
+    public $collection;
 
-	public function __construct( $root_path ) {
-		$this->root_path = $root_path;
-		$this->file_list = [];
-	}
+    public function __construct()
+    {
+        $this->collection = [];
+    }
 
-	public function add( $file ) {
-		$this->file_list[] = $file;
-	}
-
-	public function draw() {
-		$list = '<ul class="rolldown-list" id="myList">';
-
-		foreach ( $this->file_list as $f_name ) {
-			$list .= '<li>';
-			$file_path       = $this->root_path . '/' . $f_name;
-			$friendly_name   = $this->get_friendly_name( $f_name );
-			$this->imagery[] = str_replace( '.', ' ', $friendly_name . '.jpg' );
-			$list .= '<a href="' . $file_path . '" download><button class="download">' . $friendly_name . '</button></a>';
-			$list .= '</li >';
-		}
-		$list .= '</ul >';
-
-		return $list;
-	}
-
-	public function draw_old() {
-		$list = '<ul class="rolldown-list" id="myList">';
-
-		foreach ( $this->file_list as $file_name ) {
-			$list .= '<li>';
-			$file_path       = $this->root_path . '/' . $file_name;
-			$friendly_name   = $this->get_friendly_name( $file_name );
-			$this->imagery[] = str_replace( '.', ' ', $friendly_name . '.jpg' );
-			$list .= $friendly_name;
-			$list .= '<a href="' . $file_path . '" download>' . '<button class="download">Save</button></a>';
-			$list .= '</li >';
-		}
-		$list .= '</ul >';
-
-		return $list;
-	}
-
-	private function get_friendly_name( $name ) {
-
-		$name = str_replace( 'mkv', '', $name );
-		$name = str_replace( '.', ' ', $name );
-		$name = str_replace( 'mp4', ' ', $name );
-
-		return $name;
-	}
-
-	public function getImagery() {
-		return $this->imagery;
-	}
-}
-
-class VideoCollection {
-	public $collection;
-
-	public function __construct() {
-		$this->collection = [];
-	}
-
-	public function add( $file_list ) {
-		$this->collection[] = $file_list;
-	}
+    public function add($file_list)
+    {
+        $this->collection[] = $file_list;
+    }
 }
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-
 require_once __DIR__ . '/fs.php';
+
 $vid_data = create_meta();
-$files1 = $vid_data->collection[0]->draw();
-$files2 = $vid_data->collection[1]->draw();
-$files3 = $vid_data->collection[2]->draw();
+$files1   = $vid_data->collection[0]->draw();
+$files2   = $vid_data->collection[1]->draw();
+$files3   = $vid_data->collection[2]->draw();
 
 
 echo '<html><head>
@@ -18,14 +18,20 @@ echo '<html><head>
 		<meta name="msapplication-TileImage" content="http://i2.wp.com/www.cpsharp.net/wp-content/uploads/2015/09/cropped-cpsharp.logo_.300.white_.png?fit=270%2C270" />		
     </head>  
     <body><main>
-        <aside class="logo"></aside>';
-		/*<figure style-="margin-top: 1em;">
-            <div class="front" class="">Movies</div>
-            <div class="right">Movies</div>
-            <div class="left">Movies</div>
-            <div class="bottom">Movies</div>
-        </figure>
-        <aside class="glitz glitz-hide"></aside>';
+    <aside class="logo"></aside></section></div>
+        <div><aside class="film-reel">';
+echo $files3;
+echo '</aside></div></main></body>';
+echo '<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>';
+echo '<script src="page.js"></script></html>';
+
+/*<figure style-="margin-top: 1em;">
+	<div class="front" class="">Movies</div>
+	<div class="right">Movies</div>
+	<div class="left">Movies</div>
+	<div class="bottom">Movies</div>
+</figure>
+<aside class="glitz glitz-hide"></aside>';
 /*echo '<div class="list_container" id="S1_list">';
 echo $files1;
 echo '</div>';
@@ -36,21 +42,15 @@ echo '<div class="list_container" id="Movies_list">';
 echo $files3;
 echo '</div><div><section>';
 /*echo '<figure ID="S1">
-        <div class="front">Season 1</div>
-        <div class="right">Season 1</div>
-        <div class="left">Season 1</div>
-        <div class="bottom">Season 1</div>
-    </figure>';
+<div class="front">Season 1</div>
+<div class="right">Season 1</div>
+<div class="left">Season 1</div>
+<div class="bottom">Season 1</div>
+</figure>';
 echo '</div>
-		<figure ID="S2">
-            <div class="front">Season 2</div>
-            <div class="right">Season 2</div>
-            <div class="left">Season 2</div>
-            <div class="bottom">Season 2</div>
-        </figure>';*/
-
-echo '</section></div><div><aside class="film-reel">';
-echo $files3;
-echo '</aside></div></main></body>';
-echo '<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>';
-echo '<script src="page.js"></script></html>';
+<figure ID="S2">
+	<div class="front">Season 2</div>
+	<div class="right">Season 2</div>
+	<div class="left">Season 2</div>
+	<div class="bottom">Season 2</div>
+</figure>';*/
